daad633aca7918e083fcf13ec6293526ba2b128d
royFloresNyc <royfloresnyc@gmail.com>
2021-01-16 21:26:13 -0500

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Container, Col, Row} from 'react-bootstrap'
 
 import SearchBar from './Components/SearchBar';
 import Results from './Components/Results';
+import { OMDB_KEY } from './keys';
 
 
 function App() {
     const [searchVal, setSearchVal] = useState('')
+    const [result, setResult] = useState('')
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the SHOPPIES</h1>
-      </header>
-      <SearchBar handleInputChange={(val) => setSearchVal(val)} value={searchVal}/>
-      <Container>
-          <Row>
-              <Col sm={6}>
-                <Results/>
-              </Col>
-              <Col sm={6}>
-                <p>{searchVal}</p>
-              </Col>
-          </Row>
-      </Container>
-    </div>
-  );
+    const fetchMovies = () => {
+        fetch(`http://www.omdbapi.com/?s=${encodeURI(searchVal)}&apikey=${OMDB_KEY}`)
+        .then(resp => resp.json())
+        .then(data => setResult(data.Search))
+        .catch(error => console.log(error))
+    }
+    
+    useEffect(fetchMovies, [searchVal])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to the SHOPPIES</h1>
+            </header>
+            <SearchBar handleInputChange={(val) => setSearchVal(val)} value={searchVal}/>
+            <Container>
+                <Row>
+                    <Col sm={6}>
+                        <Results searchVal={searchVal} movies={result}/>
+                    </Col>
+                    <Col sm={6}>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 
-const Results = () => {
+const Results = ({ searchVal, movies}) => {
+
+    const renderMovies = () => {
+        return movies.map((movie => {
+            return (
+                <div className='border'>
+                    <h5>{movie.Title}</h5>
+                    <h5>({movie.Year})</h5>
+                </div>
+            )
+        }))
+    }
+
     return (
         <Container className='border'>
-
+            <Row>
+                <h3>{`Results for ${searchVal ? `"${searchVal}"` : '...'}`}</h3>
+            </Row>
+            <Row>
+                {movies ? renderMovies() : null}
+            </Row>
         </Container>
     )
 }
